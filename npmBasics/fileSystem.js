7d55228b789e92eab21624e123c243be43357355
utkarsh-2706 <sharmautkarsh2706@gmail.com>
2025-10-24 14:20:20 +0530

    update 24-10-2025

--- npmBasics/fileSystem.js
+++ npmBasics/fileSystem.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-// fs.writeFile('message.txt', 'Hello, Node.js!', (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
+fs.writeFile('message.txt', 'Hello, Node.js!', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
 
 // fs.readFile('message.txt', 'utf8', (err, data) => {
 //   if (err) throw err;
@@ -40,10 +40,10 @@ const fs = require('fs');
 //   console.log('The copied file has been deleted!');
 // }   );
 
-fs.rmdir('testDir', { recursive: true }, (err) => {
-  if (err) throw err;
-  console.log('The directory has been removed!');
-});
+// fs.rmdir('testDir', { recursive: true }, (err) => {
+//   if (err) throw err;
+//   console.log('The directory has been removed!');
+// });
 
 
 // fs.mkdir('testDir', (err) => {
